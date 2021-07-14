d580ab79d90084767f31e63a449412464fbed719
Vyacheslav G <vyagrankin@gmail.com>
2021-07-14 15:31:18 +0300

    Redirect back to previous page after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,17 @@ class SessionsController < ApplicationController
   before_action :skip_authorization
 
   # GET /sessions/new
-  def new; end
+  def new
+    set_return_to
+  end
 
   # POST /sessions
   def create
     user = User::Authenticate.new(session: session).call(params: params.require(:session))
 
     if user
-      redirect_to :root, notice: 'Logged in successfully'
+      return_to = session[:return_to].presence || root_path
+      redirect_to return_to, notice: 'Logged in successfully'
     else
       flash.now.alert = 'Authentication failed'
       render :new, status: :unprocessable_entity
@@ -23,5 +26,16 @@ class SessionsController < ApplicationController
     reset_session
 
     redirect_to :root, notice: 'Logged out'
+  end
+
+  private
+
+  def set_return_to
+    referer = URI(request.headers[:referer])
+    session[:return_to] = if referer.host == request.host
+                            referer.query.present? ? "#{referer.path}?#{referer.query}" : referer.path
+                          else
+                            root_path
+                          end
   end
 end
